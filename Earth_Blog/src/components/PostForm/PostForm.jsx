af03a9aaf9781dff840626856a46631aa2d0bff6
Nick0086 <nikunjkalathiya86@gmail.com>
2024-02-07 15:21:22 +0530

    c

--- Earth_Blog/src/components/PostForm/PostForm.jsx
+++ Earth_Blog/src/components/PostForm/PostForm.jsx
@@ -62,158 +62,164 @@ function PostForm({ editPost }) {
                     SetLoading(true)
                 })
         } else {
-            axios.post(`${import.meta.env.VITE_URL}/posts/create`, formData,{
-                headers: {
-                    "Content-Type": "multipart/form-data", // Set Content-Type header for FormData
-                    "Access-Control-Allow-Origin": "https://earths-jdx7.vercel.app/"
-                }
-            })
-                .then((res) => {
-                    if (res) {
-                        navigate(`/post/${res.data.data._id}`);
+            try {
+                axios.post(`${import.meta.env.VITE_URL}/posts/create`, formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data", // Set Content-Type header for FormData
+                        "Access-Control-Allow-Origin": "https://earths-jdx7.vercel.app/"
                     }
                 })
-                .catch((err) => notify(err.response))
-                .finally(() => {
-                    setImagePreview('')
-                    SetLoading(true)
-                })
-        }
-    }
+                    .then((res) => {
+                        if (res) {
+                            navigate(`/post/${res.data.data._id}`);
+                        }
+                    })
+                    .catch((err) => notify(err.response))
+                    .finally(() => {
+                        setImagePreview('')
+                        SetLoading(true)
+                    })
+            }
 
-    // function for display image preview
-    const onImageChange = (e) => {
-        if (e.target.files && e.target.files[0]) {
-            let img = e.target.files[0];
-            setImagePreview(URL.createObjectURL(img));
-            setIsImageChange(true);
+            catch (error) {
+                console.log(error)
+            }
         }
-    };
 
-    // fuction for send notification error in post data fill
-    const notify = () => {
-        if (Object.keys(errors).length !== 0) {
-            toast.error(`
+
+        // function for display image preview
+        const onImageChange = (e) => {
+            if (e.target.files && e.target.files[0]) {
+                let img = e.target.files[0];
+                setImagePreview(URL.createObjectURL(img));
+                setIsImageChange(true);
+            }
+        };
+
+        // fuction for send notification error in post data fill
+        const notify = () => {
+            if (Object.keys(errors).length !== 0) {
+                toast.error(`
                         ${errors.Title ? errors.Title.message :
-                    errors.Content ? errors.Content.message :
-                        errors.Featureimage ? errors.Featureimage.message : ""}
+                        errors.Content ? errors.Content.message :
+                            errors.Featureimage ? errors.Featureimage.message : ""}
                         `, {
-                position: "top-right",
-                autoClose: 1000,
-                pauseOnHover: true,
+                    position: "top-right",
+                    autoClose: 1000,
+                    pauseOnHover: true,
+                }
+                );
             }
-            );
         }
+
+        useEffect(() => {
+            // prevent unauthorize user for edit post
+            if (editPost) {
+                if (userData._id !== editPost.userId._id) {
+                    navigate('/')
+                }
+                setImagePreview(editPost.Featureimage)
+            }
+            notify()
+        }, [errors]);
+
+        return (
+            <>
+                {
+                    loading ?
+                        <div className='bg-gray-100 post-bg' >
+                            <ToastContainer />
+                            <div className='lg:py-24 md:py-12 py-10 container' >
+                                <form className=' lg:w-3/4 bg-white bg-opacity-60 shadow-xl backdrop-blur-sm   md:w-[90%] w-[95%] rounded-2xl mx-auto md:p-8 p-4' onSubmit={handleSubmit(postHandler)} >
+                                    <h3 className='md:text-2xl text-xl font-medium text-dark-green text-center mb-10'>
+                                        {
+                                            editPost ? "Edit Post " : "Create New Post "
+                                        }
+                                    </h3>
+                                    <div>
+                                        <Input
+                                            label="Title"
+                                            labelclass="text-[16px]"
+                                            type="text"
+                                            placeholder="Title"
+                                            {...register("Title",
+                                                {
+                                                    required: "Title is required",
+                                                    minLength: {
+                                                        value: 10,
+                                                        message: "The title must be at least 10 characters long."
+                                                    },
+                                                    maxLength: {
+                                                        value: 120,
+                                                        message: 'Title cannot exceed 200 characters',
+                                                    }
+                                                })}
+                                        />
+                                        <RTE
+                                            label="Content :"
+                                            labelclass="text-[16px]"
+                                            name="Content"
+                                            control={control}
+                                            defaultValue={editPost?.Content || ''}
+                                            {...register("Content",
+                                                {
+                                                    required: "Content is required",
+                                                    minLength: {
+                                                        value: 100,
+                                                        message: "Your content should contain at least 100 characters.",
+                                                    },
+                                                    maxLength: {
+                                                        value: 3000,
+                                                        message: 'content cannot exceed 3000 characters',
+                                                    }
+                                                }
+                                            )}
+                                        />
+                                        <Input
+                                            label="Featured Image"
+                                            labelclass="text-[16px]"
+                                            placeholder="Featured Image"
+                                            type="file"
+                                            name="file"
+                                            classname="border-b-0"
+                                            accept="image/png, image/jpg, image/jpeg, image/gif"
+                                            {...register("file", {
+                                                onChange: (e) => onImageChange(e),
+                                                ...(editPost ? {} : {
+                                                    required: "Post Thumbnail is required",
+                                                }),
+                                            })}
+                                        />
+                                        {imagePreview &&
+                                            <div className="my-4 ">
+                                                <img src={imagePreview} alt="Featured Image" srcSet="" className="md:w-[50%] w-[95%] h-auto mx-auto rounded-lg shadow-xl" />
+                                            </div>
+                                        }
+                                        <Selector
+                                            labelclass="text-[16px]"
+                                            label="Status"
+                                            options={["Active", "Inactive"]}
+                                            className="mb-6"
+                                            {...register("status", { required: true })}
+                                        />
+                                        <Selector
+                                            label="Category"
+                                            labelclass="text-[16px]"
+                                            options={["Personal", "News", "Sport", "Travel", "Food", "Fashion", "Finance", "Music", "Business", "Lifesyle"]}
+                                            className="mb-6 "
+                                            {...register("Category", { required: true })}
+                                        />
+                                        <div className='text-center mt-6'>
+                                            <Button type='submit' onClick={notify} >{editPost ? "Edit Post" : "Add Post"}</Button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        : <Spinner />
+                }
+            </>
+        )
     }
 
-    useEffect(() => {
-        // prevent unauthorize user for edit post
-        if (editPost) {
-            if (userData._id !== editPost.userId._id) {
-                navigate('/')
-            }
-            setImagePreview(editPost.Featureimage)
-        }
-        notify()
-    }, [errors]);
-
-    return (
-        <>
-            {
-                loading ?
-                    <div className='bg-gray-100 post-bg' >
-                        <ToastContainer />
-                        <div className='lg:py-24 md:py-12 py-10 container' >
-                            <form className=' lg:w-3/4 bg-white bg-opacity-60 shadow-xl backdrop-blur-sm   md:w-[90%] w-[95%] rounded-2xl mx-auto md:p-8 p-4' onSubmit={handleSubmit(postHandler)} >
-                                <h3 className='md:text-2xl text-xl font-medium text-dark-green text-center mb-10'>
-                                    {
-                                        editPost ? "Edit Post " : "Create New Post "
-                                    }
-                                </h3>
-                                <div>
-                                    <Input
-                                        label="Title"
-                                        labelclass="text-[16px]"
-                                        type="text"
-                                        placeholder="Title"
-                                        {...register("Title",
-                                            {
-                                                required: "Title is required",
-                                                minLength: {
-                                                    value: 10,
-                                                    message: "The title must be at least 10 characters long."
-                                                },
-                                                maxLength: {
-                                                    value: 120,
-                                                    message: 'Title cannot exceed 200 characters',
-                                                }
-                                            })}
-                                    />
-                                    <RTE
-                                        label="Content :"
-                                        labelclass="text-[16px]"
-                                        name="Content"
-                                        control={control}
-                                        defaultValue={editPost?.Content || ''}
-                                        {...register("Content",
-                                            {
-                                                required: "Content is required",
-                                                minLength: {
-                                                    value: 100,
-                                                    message: "Your content should contain at least 100 characters.",
-                                                },
-                                                maxLength: {
-                                                    value: 3000,
-                                                    message: 'content cannot exceed 3000 characters',
-                                                }
-                                            }
-                                        )}
-                                    />
-                                    <Input
-                                        label="Featured Image"
-                                        labelclass="text-[16px]"
-                                        placeholder="Featured Image"
-                                        type="file"
-                                        name="file"
-                                        classname="border-b-0"
-                                        accept="image/png, image/jpg, image/jpeg, image/gif"
-                                        {...register("file", {
-                                            onChange: (e) => onImageChange(e),
-                                            ...(editPost ? {} : {
-                                                required: "Post Thumbnail is required",
-                                            }),
-                                        })}
-                                    />
-                                    {imagePreview &&
-                                        <div className="my-4 ">
-                                            <img src={imagePreview} alt="Featured Image" srcSet="" className="md:w-[50%] w-[95%] h-auto mx-auto rounded-lg shadow-xl" />
-                                        </div>
-                                    }
-                                    <Selector
-                                        labelclass="text-[16px]"
-                                        label="Status"
-                                        options={["Active", "Inactive"]}
-                                        className="mb-6"
-                                        {...register("status", { required: true })}
-                                    />
-                                    <Selector
-                                        label="Category"
-                                        labelclass="text-[16px]"
-                                        options={["Personal", "News", "Sport", "Travel", "Food", "Fashion", "Finance", "Music", "Business", "Lifesyle"]}
-                                        className="mb-6 "
-                                        {...register("Category", { required: true })}
-                                    />
-                                    <div className='text-center mt-6'>
-                                        <Button type='submit' onClick={notify} >{editPost ? "Edit Post" : "Add Post"}</Button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    : <Spinner />
-            }
-        </>
-    )
-}
-
-export default PostForm;
+    export default PostForm;
